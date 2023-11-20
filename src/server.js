6f9867a2d120ed40798f8a7f34cd503984733502
xandguima <xan.96@hotmail.com>
2023-11-20 17:16:37 -0300

    config start archive database , routes and errors

---
+++ src/server.js
@@ -0,0 +1,34 @@
+require("express-async-errors");
+const express =require("express");
+const AppError=require("./utils/AppError.js")
+const sqliteConnection=require("./database/sqlite")
+
+
+const app = express();
+app.use(express.json());
+
+sqliteConnection()
+
+const routes=require("./routes")
+
+app.use(routes);
+
+app.use((error,request,response, next)=>{
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({
+      status:"error",
+      message:error.message
+    })
+  };
+
+  return response.status(500).json({
+    status:"error",
+    message:" internal server error"
+  })
+});
+
+const PORT = 3333;
+
+app.listen(PORT,()=>{
+  console.log(`Server is running on port ${PORT}`);
+});
