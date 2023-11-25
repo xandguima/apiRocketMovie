284a619e861cfc570115cda552241aef286edb3a
xandguima <xan.96@hotmail.com>
2023-11-25 11:10:05 -0300

    create routes for notes for functions

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,15 @@
+const {Router}=require("express");
+const NotesController=require("../controllers/notesMovieController")
+
+const notesController = new NotesController()
+
+const notesRoutes=Router();
+
+notesRoutes.get("/", notesController.index);
+notesRoutes.post("/:user_id",notesController.create)
+notesRoutes.get("/:id",notesController.show)
+notesRoutes.delete("/:id",notesController.delete)
+
+
+
+module.exports=notesRoutes
