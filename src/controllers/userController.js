2f6b2f05861a492ddc6105ec6f20d509b7866fe5
xandguima <xan.96@hotmail.com>
2023-11-25 11:07:23 -0300

    adaptation for checkExistUser and new functions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 const knex=require("../database/knex")
-const{hash}=require("bcryptjs")
+const{hash, compare}=require("bcryptjs")
 const AppError = require("../utils/AppError");
 
 class UsersController{
@@ -7,11 +7,10 @@ class UsersController{
     const {name, email, password}=request.body;
 
    const checkExistUser= await knex("users").where({email})
-    console.log(checkExistUser)
-   if(checkExistUser){
-    throw new AppError("Este e-mail já está em uso");
-   }
 
+    if(checkExistUser.length!=0){
+     throw new AppError("Este email já esta em uso")
+    }
     const cryptPassword=await hash(password,8)
    
     await knex("users").insert({
@@ -22,6 +21,70 @@ class UsersController{
 
     return response.status(201).json();
   }
+
+  async update(request,response){
+    const{name,email,oldPassword,newPassword}=request.body
+    const{id}=request.params
+
+    const user= await knex("users").where({id})
+
+    if(user.length==0){
+      throw new AppError("Usuário não encontrado")
+    }
+  
+    if(email){
+     const checkEmail = await knex("users").where({email})
+      if(checkEmail.length!==0 && checkEmail[0].id!==user[0].id){
+        throw new AppError("Este e-mail já está em uso")
+      }
+    }
+    
+    user[0].name = name ?? user[0].name
+    user[0].email = email ?? user[0].email
+    
+
+    if(newPassword && !oldPassword){
+      throw new AppError("Para redefinir a senha é necessario informar a senha antiga")
+    }
+    if(!newPassword && oldPassword){
+     throw new AppError("Para redefinir a senha é necessario informar a nova senha")
+    }
+    console.log(user[0].password)
+    if(newPassword && oldPassword){
+      const checkPassword= await compare(oldPassword,user[0].password)
+      if(!checkPassword){
+        throw new AppError("A senha antiga não confere")
+      }
+      user[0].password= await hash(newPassword,8)
+    }
+    
+
+    await knex("users").where({id}).update({
+      name:user[0].name,
+      email:user[0].email,
+      password:user[0].password,
+      updated_at:knex.fn.now()
+    })
+
+    return response.status(200).json();
+
+  }
+
+  async delete(request,response){
+    const {id}=request.params
+
+    const checkUser=await knex("users").where({id})
+
+    if(checkUser.length==0){
+      throw new AppError("Usuário não encontrado")
+    }
+    await knex("users").where({id}).delete()
+  
+
+    return response.json()
+    
+  }
+
 
   
 }
