cbbf03ae226cc3d859ad78296baaaff2b615f5b3
xandguima <xan.96@hotmail.com>
2023-12-25 22:30:45 -0300

    index routes add sessions

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const routes=Router();
 
 const userRoutes=require("./users.routes")
 const notesRoutes=require("./notes.routes")
+const sessionRoutes=require("./sessions.routes")
 
 
 routes.use("/users",userRoutes)
 routes.use("/notes",notesRoutes)
+routes.use("/sessions",sessionRoutes)
 
 module.exports=routes
