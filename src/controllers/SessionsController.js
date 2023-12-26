31731899a8aea4acb937b94d5bdaa28c61d06f48
xandguima <xan.96@hotmail.com>
2023-12-25 22:29:45 -0300

    check auth sessions

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,37 @@
+const knex =require('../database/knex')
+const{compare}=require("bcryptjs")
+const AppError= require("../utils/AppError")
+const authConfig=require("../config/auth")
+const {sign}= require("jsonwebtoken")
+
+class SessionsController {
+  async create(request,response){
+    const {email, password}= request.body
+
+
+    const user= await knex("users").where({email}).first()
+   
+    
+    if(!user){
+      throw new AppError("Email e/ou senha incorreta",401)
+    }
+    
+    
+    const checkPassword= await compare(password,user.password)
+    if(!checkPassword){
+      throw new AppError("Email e/ou senha incorreta",401)
+       
+    }
+    const {secret, expiresIn}=authConfig.jwt
+    const token=sign({},secret,{
+      subject: String(user.id),
+      expiresIn
+    })
+
+    return response.status(200).json({user,token});
+   
+  }
+
+}
+
+module.exports= SessionsController;
